67d02b48c613dc23db3021a70a8ff29aef828877
yenashin <yena5790@gachon.ac.kr>
2023-01-27 20:47:18 +0900

    [REFACTOR] SRT-124 알림톡 컬럼에 버튼 추가, 기존 다대일 매핑된 버튼 엔티티 삭제

--- src/main/java/com/srt/message/repository/KakaoButtonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.srt.message.repository;
-
-import com.srt.message.domain.KakaoButton;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface KakaoButtonRepository extends JpaRepository<KakaoButton, Long> {
-}
