b28eddffe0d2c87d1207a5bfb98aa8d72a6f0725
yenashin <yena5790@gachon.ac.kr>
2023-02-02 11:44:51 +0900

    [REFACTOR] SRT-95 알림톡 중계사 발송 비율 조회 시 발송 비율이 설정되어 있지 않다면 Default 값으로 저장하기

--- src/main/java/com/srt/message/repository/KakaoMessageRuleRepository.java
+++ src/main/java/com/srt/message/repository/KakaoMessageRuleRepository.java
@@ -2,15 +2,10 @@ package com.srt.message.repository;
 
 import com.srt.message.config.status.BaseStatus;
 import com.srt.message.domain.KakaoMessageRule;
-import com.srt.message.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface KakaoMessageRuleRepository extends JpaRepository<KakaoMessageRule, Long> {
-    @Query("select km from KakaoMessageRule km left join fetch km.kakaoBroker order by km.kakaoBroker.name asc")
-    List<KakaoMessageRule> findAllByMember(Member member);
-
     List<KakaoMessageRule> findByMemberIdAndStatus(Long memberId, BaseStatus status);
 }
