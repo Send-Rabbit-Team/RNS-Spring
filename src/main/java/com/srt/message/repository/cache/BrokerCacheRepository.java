c47f99458ddf7e1d0d5a3ed8f4b7dee7a5ba9b5d
yenashin <yena5790@gachon.ac.kr>
2023-02-03 11:40:56 +0900

    [FEAT] SRT-208 알림톡 Listener 구현 후 알림톡 발송 결과 저장

--- src/main/java/com/srt/message/repository/cache/BrokerCacheRepository.java
+++ src/main/java/com/srt/message/repository/cache/BrokerCacheRepository.java
@@ -2,7 +2,9 @@ package com.srt.message.repository.cache;
 
 import com.srt.message.config.exception.BaseException;
 import com.srt.message.domain.Broker;
+import com.srt.message.domain.KakaoBroker;
 import com.srt.message.repository.BrokerRepository;
+import com.srt.message.repository.KakaoBrokerRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
@@ -14,11 +16,17 @@ import static com.srt.message.config.response.BaseResponseStatus.NOT_EXIST_BROKE
 @Component
 @RequiredArgsConstructor
 public class BrokerCacheRepository {
+    private final KakaoBrokerRepository kakaoBrokerRepository;
 
     private final BrokerRepository brokerRepository;
 
     @Cacheable(value = "Broker", key = "#brokerId")
     public Broker findBrokerById(long brokerId){
         return brokerRepository.findById(brokerId).orElseThrow(() -> new BaseException(NOT_EXIST_BROKER));
+    }
+
+    @Cacheable(value = "KakaoBroker", key = "#brokerId")
+    public KakaoBroker findKakaoBrokerById(long brokerId){
+        return kakaoBrokerRepository.findById(brokerId).orElseThrow(() -> new BaseException(NOT_EXIST_BROKER));
     }
 }
