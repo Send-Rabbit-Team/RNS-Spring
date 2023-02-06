04033c99c7ebfca27c31860ef999d53eb6074a65
yenashin <yena5790@gachon.ac.kr>
2023-02-06 17:51:15 +0900

    [FEAT] SRT-203 알림톡 예약 발송에 필요한 entity 생성

---
+++ src/main/java/com/srt/message/repository/ReserveKakaoMessageRepository.java
@@ -0,0 +1,7 @@
+package com.srt.message.repository;
+
+import com.srt.message.domain.ReserveKakaoMessage;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ReserveKakaoMessageRepository extends CrudRepository<ReserveKakaoMessage, Long> {
+}
