694325d6303de37d9ebcef6722ca764f83fc8005
yenashin <yena5790@gachon.ac.kr>
2023-02-06 17:52:34 +0900

    [FEAT] SRT-242 알림톡 발송 조회 서비스 구현

--- src/main/java/com/srt/message/repository/KakaoMessageRepository.java
+++ src/main/java/com/srt/message/repository/KakaoMessageRepository.java
@@ -1,13 +1,39 @@
 package com.srt.message.repository;
 
+import com.srt.message.config.type.ButtonType;
+import com.srt.message.config.type.MessageType;
 import com.srt.message.domain.KakaoMessage;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface KakaoMessageRepository extends JpaRepository<KakaoMessage, Long> {
-    // 캐시용
     @EntityGraph(value = "KakaoMessage.with.Member.SenderNumber.RepeatRule")
     Optional<KakaoMessage> findKakaoMessageById(long messageId);
+
+    @Query(value = "select km from KakaoMessage km where km.member.id = :memberId",
+            countQuery = "select count(km) from KakaoMessage km where km.member.id = :memberId")
+    Page<KakaoMessage> findAllKakaoMessage(Pageable pageable, Long memberId);
+
+    @Query(value = "select km from KakaoMessage km where km.member.id = :memberId and km.buttonType = :buttonType",
+            countQuery = "select count(km) from KakaoMessage km where km.member.id = :memberId and km.buttonType = :buttonType")
+    Page<KakaoMessage> findKakaoMessageByButtonType(Pageable pageable, Long memberId, ButtonType buttonType);
+
+    @Query(value = "select kmr from KakaoMessageResult kmr where kmr.kakaoMessage.member.id = :memberId and kmr.contact.phoneNumber = :contactNumber",
+            countQuery = "select count(kmr) from KakaoMessageResult kmr where kmr.kakaoMessage.member.id = :memberId and kmr.contact.phoneNumber = :contactNumber")
+    Page<KakaoMessage> findKakaoMessageByContactNumber(Pageable pageable, Long memberId, String contactNumber);
+
+    @Query(value = "select kmr from KakaoMessageResult kmr where kmr.kakaoMessage.member.id = :memberId and kmr.contact.memo = :contactMemo",
+            countQuery = "select count(kmr) from KakaoMessageResult kmr where kmr.kakaoMessage.member.id = :memberId and kmr.contact.memo = :contactMemo")
+    Page<KakaoMessage> findKakaoMessageByContactMemo(Pageable pageable, Long memberId, String contactMemo);
+
+    @Query(value = "select km from KakaoMessage km where km.member.id = :memberId and km.title like %:messageContent% or km.subTitle like %:messageContent% or " +
+            "km.content like %:messageContent% or km.description like %:messageContent%",
+            countQuery = "select count(km) from KakaoMessage km where km.member.id = :memberId and km.title like %:messageContent% or km.subTitle like %:messageContent% or " +
+            "km.content like %:messageContent% or km.description like %:messageContent%")
+    Page<KakaoMessage> findKakaoMessageByMessageContent(Pageable pageable, Long memberId, String messageContent);
 }
