97db327e12757d5fdba5549ecc1a1054a2c3b2f1
yenashin <yena5790@gachon.ac.kr>
2023-02-07 17:15:20 +0900

    [FEAT] SRT-253 알림톡 예약 발송 수신자 조회 API 구현

--- src/main/java/com/srt/message/repository/ReserveMessageContactRepository.java
+++ src/main/java/com/srt/message/repository/ReserveMessageContactRepository.java
@@ -1,5 +1,6 @@
 package com.srt.message.repository;
 
+import com.srt.message.domain.ReserveKakaoMessage;
 import com.srt.message.domain.ReserveMessage;
 import com.srt.message.domain.ReserveMessageContact;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,5 @@ import java.util.List;
 
 public interface ReserveMessageContactRepository extends JpaRepository<ReserveMessageContact, Long> {
     List<ReserveMessageContact> findAllByReserveMessage(ReserveMessage reserveMessage);
+    List<ReserveMessageContact> findAllByReserveKakaoMessage(ReserveKakaoMessage reserveKakaoMessage);
 }
