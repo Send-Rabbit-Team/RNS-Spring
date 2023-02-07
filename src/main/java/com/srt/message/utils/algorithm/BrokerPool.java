843a14d95e179f9c37aa1d7362dde7bb0c6808cc
hyeong-jun-kim <gamebox27@naver.com>
2023-02-07 16:47:43 +0900

    [REFACTOR] SRT-198 메시지 발송 리팩토링 (비율 -> 순차적으로 보냄)

--- src/main/java/com/srt/message/utils/algorithm/BrokerPool.java
+++ src/main/java/com/srt/message/utils/algorithm/BrokerPool.java
@@ -1,33 +1,62 @@
 package com.srt.message.utils.algorithm;
 
-import com.srt.message.domain.Broker;
-
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.TreeMap;
+import java.util.*;
 
 /**
  * 메시지 중계사 분배 발송 알고리즘
  * Weighted round-robin algorithm
  */
 public class BrokerPool{
-    Random random = new Random();
-    TreeMap<Integer, BrokerWeight> pool;
+    Queue<BrokerWeight> brokerQueue;
+    ArrayList<BrokerWeight> brokers;
     int totalWeight;
+    double value;
 
     public BrokerPool(ArrayList<BrokerWeight> brokers){
-        this.pool = new TreeMap<>();
-
+        this.brokers = brokers;
+        this.brokerQueue = new LinkedList<>();
         totalWeight = 0;
+
         for(BrokerWeight bw: brokers){
-            totalWeight += bw.getWeight();
-            this.pool.put(totalWeight, bw);
+            this.brokerQueue.add(bw);
+            totalWeight += bw.getTotalWeight();
         }
+
+        value = totalWeight / 100d;
     }
 
     public BrokerWeight getNext(){
-        int rnd = random.nextInt(this.totalWeight);
-        return pool.ceilingEntry(rnd).getValue();
+        BrokerWeight broker = null;
+        while(!brokerQueue.isEmpty()){
+            BrokerWeight brokerWeight = brokerQueue.poll();
+            double currentWeight = brokerWeight.getCurrentWeight();
+            if(currentWeight >= brokerWeight.getTotalWeight())
+                continue;
+
+            broker = brokerWeight;
+            broker.addCurrentWeight(value);
+            brokerQueue.add(brokerWeight);
+            break;
+        }
+
+        if(brokerQueue.isEmpty()){
+            broker = resetBrokerQueue();
+        }
+
+        return broker;
+    }
+
+    public BrokerWeight resetBrokerQueue(){
+        brokers.forEach(bw -> {
+            bw.resetCurrentWeight();
+            brokerQueue.add(bw);
+        });
+
+        BrokerWeight broker = brokerQueue.peek();
+        broker.addCurrentWeight(value);
+        brokerQueue.add(broker);
+
+        return broker;
     }
 }
 
